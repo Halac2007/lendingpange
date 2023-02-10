ebeaa0338f9561c8987cb76d076ee319ebf90785
Be Mai <bemai@gmail.com>
2023-02-10 12:10:17 +0700

    timeline

--- js/slider.js
+++ js/slider.js
@@ -1,3 +1,4 @@
+/*------slider-----------*/
 let slideIndex = 1
 showSlides(slideIndex)
 
@@ -25,3 +26,25 @@ function showSlides(n) {
 
   slides[slideIndex - 1].style.display = 'block'
 }
+
+/*--------Count Number ----------*/
+
+document.addEventListener('DOMContentLoaded', () => {
+  function counter(id, start, end, duration) {
+    let obj = document.getElementById(id),
+      current = start,
+      range = end - start,
+      increment = end > start ? 1 : -1,
+      step = Math.abs(Math.floor(duration / range)),
+      timer = setInterval(() => {
+        current += increment
+        obj.textContent = current
+        if (current == end) {
+          clearInterval(timer)
+        }
+      }, step)
+  }
+  counter('count1', 900, 15, 2500)
+  counter('count2', 900, 4, 2500)
+  counter('count3', 900, 5, 2500)
+})
