360827e560700fce5ab7a11a7a67d2e3f036fb80
Be Mai <bemai@gmail.com>
2023-02-20 09:02:31 +0700

    update page demo2

--- js/slider.js
+++ js/slider.js
@@ -1,4 +1,26 @@
 /*------slider-----------*/
+
+// let slideIndex = 0
+// showSlides()
+
+// function showSlides() {
+//   let i
+//   let slides = document.getElementsByClassName('mySlides')
+
+//   for (i = 0; i < slides.length; i++) {
+//     slides[i].style.display = 'none'
+//   }
+//   slideIndex++
+//   if (slideIndex > slides.length) {
+//     slideIndex = 1
+//   }
+
+//   slides[slideIndex - 1].style.display = 'block'
+
+//   // setTimeout(showSlides, 0) // Change image every 2 seconds
+// }
+
+/*---------------*/
 let slideIndex = 1
 showSlides(slideIndex)
 
