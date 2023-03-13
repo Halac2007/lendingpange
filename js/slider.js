f338c046ca392a0e44ad741920a2e94de959ced6
Be Mai <bemai@gmail.com>
2023-03-13 16:34:51 +0700

    add number

--- js/slider.js
+++ js/slider.js
@@ -51,22 +51,22 @@ function showSlides(n) {
 
 /*--------Count Number ----------*/
 
-document.addEventListener('DOMContentLoaded', () => {
-  function counter(id, start, end, duration) {
-    let obj = document.getElementById(id),
-      current = start,
-      range = end - start,
-      increment = end > start ? 1 : -1,
-      step = Math.abs(Math.floor(duration / range)),
-      timer = setInterval(() => {
-        current += increment
-        obj.textContent = current
-        if (current == end) {
-          clearInterval(timer)
-        }
-      }, step)
-  }
-  counter('count1', 90, 15, 1500)
-  counter('count2', 90, 4, 1500)
-  counter('count3', 90, 5, 1500)
-})
+// document.addEventListener('DOMContentLoaded', () => {
+//   function counter(id, start, end, duration) {
+//     let obj = document.getElementById(id),
+//       current = start,
+//       range = end - start,
+//       increment = end > start ? 1 : -1,
+//       step = Math.abs(Math.floor(duration / range)),
+//       timer = setInterval(() => {
+//         current += increment
+//         obj.textContent = current
+//         if (current == end) {
+//           clearInterval(timer)
+//         }
+//       }, step)
+//   }
+//   counter('count1', 90, 15, 1500)
+//   counter('count2', 90, 4, 1500)
+//   counter('count3', 90, 5, 1500)
+// })
