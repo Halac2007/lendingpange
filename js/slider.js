d6eb9019cabaa34235dcc4be290c0cf9f7e96268
Be Mai <bemai@gmail.com>
2023-03-15 11:31:25 +0700

    update code

--- js/slider.js
+++ js/slider.js
@@ -1,53 +1,62 @@
 /*------slider-----------*/
 
-// let slideIndex = 0
-// showSlides()
+let slideIndex = 0
+showSlides()
 
-// function showSlides() {
-//   let i
-//   let slides = document.getElementsByClassName('mySlides')
-
-//   for (i = 0; i < slides.length; i++) {
-//     slides[i].style.display = 'none'
-//   }
-//   slideIndex++
-//   if (slideIndex > slides.length) {
-//     slideIndex = 1
-//   }
-
-//   slides[slideIndex - 1].style.display = 'block'
-
-//   // setTimeout(showSlides, 0) // Change image every 2 seconds
-// }
-
-/*---------------*/
-let slideIndex = 1
-showSlides(slideIndex)
-
-function plusSlides(n) {
-  showSlides((slideIndex += n))
-}
-
-function currentSlide(n) {
-  showSlides((slideIndex = n))
-}
-
-function showSlides(n) {
+function showSlides() {
   let i
   let slides = document.getElementsByClassName('mySlides')
 
-  if (n > slides.length) {
-    slideIndex = 1
-  }
-  if (n < 1) {
-    slideIndex = slides.length
-  }
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = 'none'
   }
+  slideIndex++
+  if (slideIndex > slides.length) {
+    slideIndex = 1
+  }
 
   slides[slideIndex - 1].style.display = 'block'
+
+  setTimeout(showSlides, 5000)
 }
+
+/*-------------------------*/
+const caccordContent = document.querySelectorAll('.box-show')
+
+for (i = 0; i < caccordContent.length; i++) {
+  caccordContent[i].addEventListener('click', function () {
+    this.classList.toggle('active')
+  })
+}
+
+/*---------------*/
+// let slideIndex = 1
+// showSlides(slideIndex)
+
+// function plusSlides(n) {
+//   showSlides((slideIndex += n))
+// }
+
+// function currentSlide(n) {
+//   showSlides((slideIndex = n))
+// }
+
+// function showSlides(n) {
+//   let i
+//   let slides = document.getElementsByClassName('mySlides')
+
+//   if (n > slides.length) {
+//     slideIndex = 1
+//   }
+//   if (n < 1) {
+//     slideIndex = slides.length
+//   }
+//   for (i = 0; i < slides.length; i++) {
+//     slides[i].style.display = 'none'
+//   }
+
+//   slides[slideIndex - 1].style.display = 'block'
+// }
 
 /*--------Count Number ----------*/
 
